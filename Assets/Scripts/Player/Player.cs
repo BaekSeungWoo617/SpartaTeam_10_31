15455889435527e2d777d5130797bbaba15a9e4a
ojs0124 <ojs_0124_@naver.com>
2024-10-31 19:12:46 +0900

    feat: Player
    - Add camera movement with player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,19 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
+        Camera _camera = Camera.main;
+
+        if (_camera != null)
+        {
+            _camera.transform.SetParent(transform);
+            _camera.transform.localPosition = new Vector3(0, 10, -6);
+            _camera.transform.localRotation = Quaternion.Euler(36, 0, 0);
+        }
+        else
+        {
+            Debug.LogError("메인 카메라를 찾을 수 없습니다.");
+        }
+
         controller = GetComponent<PlayerController>();
     }
 }
