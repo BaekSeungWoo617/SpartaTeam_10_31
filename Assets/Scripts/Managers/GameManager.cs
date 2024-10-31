0be55062ceeda007db8610e00813eeb2fe6b485d
SpartaTeam_10_31 <koke100@naver.com>
2024-10-31 14:19:52 +0900

    ScoreHuddleCount

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,51 @@
+using System;
+using Unity;
 public class GameManager : SingletonBase<GameManager>
 {
+    private int score;
+    private int huddleCount;
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+    public int HuddleCount
+    {
+        get { return huddleCount; }
+        set { huddleCount = value; }
+    }
     protected override void Awake()
     {
         base.Awake();
         DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    public void AddHuddleCount(int amount)
+    {
+        huddleCount += amount;
+    }
+
+    public void ResetHuddleCount()
+    {
+        huddleCount = 0;
+    }
+    public void ResetGame()
+    {
+        ResetScore();
+        ResetHuddleCount();
     }
 }
