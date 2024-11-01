009d667c9adeaf937cd19cf8618cb5d612da6e9f
SpartaTeam_10_31 <koke100@naver.com>
2024-11-01 21:00:18 +0900

    chore: CreateRandomRoad,Huddle

--- Assets/Scripts/Managers/RoadManager.cs
+++ Assets/Scripts/Managers/RoadManager.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 public class RoadManager : SingletonBase<RoadManager>
 {
     private ObjectPool roadPool;
+    private ObjectPool huddlePool;
+    private ObjectPool leftPool;
+    private ObjectPool rightPool;
+
     private GameObject roadPrefab;
+    private GameObject huddlePrefab;
+    private GameObject leftBDPrefab;
+    private GameObject rightBDPrefab;
+
+
     private GameObject prevRoad;
     private GameObject StartRoad;
     private GameObject EndRoad;
@@ -17,13 +26,24 @@ public class RoadManager : SingletonBase<RoadManager>
     {
         base.Awake();
 
-        SetRoadPrefab();
-        InitializeObjectPool();
+        //SetRoadPrefab();
+        roadPrefab = null;
+        SetRandomPrefab(ref roadPrefab, "Prefabs/Road");
+        huddlePrefab = null;
+        SetRandomPrefab(ref huddlePrefab, "Prefabs/Huddle");
+        leftBDPrefab = null;
+        SetRandomPrefab(ref leftBDPrefab, "Prefabs/LeftBuilding");
+        rightBDPrefab = null;
+        SetRandomPrefab(ref rightBDPrefab, "Prefabs/RightBuilding");
+
+
+        InitializeRoadObjectPool();
+        InitializeHuddleObjectPool();
     }
     private void Start()
     {
-        StartRoad = Resources.Load<GameObject>("Prefabs/Road/RoadStart");
-        EndRoad= Resources.Load<GameObject>("Prefabs/Road/RoadEnd");
+        StartRoad = Resources.Load<GameObject>("Prefabs/SpecialRoad/RoadStart");
+        EndRoad= Resources.Load<GameObject>("Prefabs/Road/Special/RoadEnd");
         GameObject newObj = Instantiate(StartRoad);
     }
     private void SetRoadPrefab()
@@ -35,11 +55,30 @@ public class RoadManager : SingletonBase<RoadManager>
             Debug.LogError("오브젝트 불러오기 애러");
         }
     }
+    private void SetRandomPrefab(ref GameObject myObject,string folderPath)// 오브젝트 종류, 오브젝트 폴더위치ex)Prefabs/Road
+    {
+        GameObject[] prefabs = Resources.LoadAll<GameObject>(folderPath);
 
-    private void InitializeObjectPool()
+        if (prefabs.Length == 0)
+        {
+            Debug.LogError("오브젝트를 불러오는 데 실패했습니다.");
+            return;
+        }
+
+        int randomIndex = Random.Range(0, prefabs.Length);
+        myObject = prefabs[randomIndex];
+        Debug.Log($"선택된 오브젝트: {myObject.name}");
+    }
+
+    private void InitializeRoadObjectPool()
     {
         roadPool = gameObject.AddComponent<ObjectPool>();
         roadPool.ObjectPrefab = roadPrefab;
+    }
+    private void InitializeHuddleObjectPool()
+    {
+        huddlePool = gameObject.AddComponent<ObjectPool>();
+        huddlePool.ObjectPrefab = huddlePrefab;
     }
 
     void Update()
@@ -48,23 +87,25 @@ public class RoadManager : SingletonBase<RoadManager>
 
         if (timer >= spawnInterval)
         {
-            SetActiveRoad();
+            SetActiveObject(roadPool, prevRoad);
+            SetActiveObject(huddlePool, huddlePrefab);
+
             timer = 0f;
         }
     }
 
-    public void SetActiveRoad()
+    public void SetActiveObject(ObjectPool objectPool, GameObject myObject)
     {
-        GameObject road = roadPool.GetObject();
+        GameObject road = objectPool.GetObject();
 
-        if (prevRoad == null)
+        if (myObject == null)
         {
             road.transform.position = Vector3.zero;
         }
         else
         {
-            Vector3 lastRoadPosition = prevRoad.transform.position;
-            Vector3 lastRoadSize = prevRoad.GetComponent<Renderer>().bounds.size;
+            Vector3 lastRoadPosition = myObject.transform.position;
+            Vector3 lastRoadSize = myObject.GetComponent<Renderer>().bounds.size;
             road.transform.position = new Vector3(lastRoadPosition.x, lastRoadPosition.y, lastRoadPosition.z + lastRoadSize.z);
         }
 
@@ -90,11 +131,11 @@ public class RoadManager : SingletonBase<RoadManager>
 
         }
         else road.transform.position = new Vector3(0, 0, 0);
-        prevRoad = road;
+        myObject = road;
     }
 
-    public void ReleaseRoad(GameObject road)
+    public void ReleaseRoad(ObjectPool objectPool,GameObject road)
     {
-        roadPool.ReleaseObject(road);
+        objectPool.ReleaseObject(road);
     }
 }
